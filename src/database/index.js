73f187770dbde7fa3ad0f5144dd35a145b4f7c32
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-04-26 11:11:45 -0300

    feat: complete create task endpoint

--- src/database/index.js
+++ src/database/index.js
@@ -23,4 +23,16 @@ export class Database {
       }
     });
   }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+
+    return data;
+  }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,27 @@
+import { Database } from "../database/index.js";
 import { buildRoutePath } from "../utils/build-route-path.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
 
 export const routes = [
   {
     method: "POST",
-    path: "/tasks",
-    handler: (req, res) => {},
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const user = database.insert("tasks", {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      });
+
+      return res.writeHead(201).end(JSON.stringify(user));
+    },
   },
 
   {
